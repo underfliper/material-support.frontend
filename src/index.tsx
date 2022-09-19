128355ce7b0d4a76aff3baecb58bb15ad7f5f593
Ruslan Mingaleev <neoch56@mail.ru>
2022-09-19 14:24:50 +0300

    Install Sass

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
 import App from './App'
-import './index.css'
+import './index.scss'
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 
