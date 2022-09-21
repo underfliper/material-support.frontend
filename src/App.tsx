9bbc119c04529200ceff5553a77988c223e32b82
Ruslan Mingaleev <neoch56@mail.ru>
2022-09-21 17:30:28 +0300

    Add routing using react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 
 const App: React.FC = () => {
-  return <Layout></Layout>
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<>Главная</>} />
+          <Route path="/applications" element={<>Управление заявлениями</>} />
+          <Route path="/admin-panel" element={<>Панель администрации</>} />
+          <Route path="/information" element={<>Информация</>} />
+          <Route path="/profile" element={<>Личный кабинет</>} />
+          <Route path="/apply" element={<>Подать заявление</>} />
+          <Route path="*" element={<h2>Страница не найдена</h2>} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  )
 }
 
 export default App
